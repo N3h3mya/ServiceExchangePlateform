959466780de096123b9c840b9a0d472a0684437d
Néhémie Mubenga Maweja <132246535+LeBlaireauMielleux@users.noreply.github.com>
2024-06-23 19:48:34 +0200

    Create login.php
    adding the file login.php

---
+++ php/login.php
@@ -0,0 +1,25 @@
+<?php
+header('content-type: application/xhtml+xml;charset=utf-8');
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <!--
+    Identité              : 
+    Date premier encodage :
+  -->
+  <meta charset="utf-8" />
+  <title>Ce texte apparaît dans la barre de titre du navigateur</title>
+  <style type="text/css">
+  	/* du code CSS */ 
+  </style>
+  <script type="text/javascript">
+  <![CDATA[
+  	// du code javascript
+  ]]>
+  </script>
+</head>
+<body>
+    <input type="text" />
+</body>
+</html>
